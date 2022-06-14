41db0138c891de95bd2f83014104391a07731539
graywolf <graywolf186@gmail.com>
2022-06-14 20:18:27 +0800

    1

--- publish.ps1
+++ publish.ps1
@@ -13,20 +13,15 @@ git add .
 $shouldUpdate = git status -s
 if ($shouldUpdate) {
   git commit -m "Publish"
-  git remote set-url origin "https://tadaf:$token@github.com/bs-community/plugins-dist.git"
+  git remote set-url origin "https://tadaf:$token@github.com/graytoowolf/plugins-dist.git"
   git push origin master
 }
 
 Set-Location '..'
 
-$botRelease = (Invoke-WebRequest 'https://api.github.com/repos/bs-community/telegram-bot/releases/latest').Content | ConvertFrom-Json
-$botBinUrl = ((Invoke-WebRequest $botRelease.assets_url).Content | ConvertFrom-Json).browser_download_url
 
-bash -c "curl -fSL $botBinUrl -o bot"
-chmod +x ./bot
-./bot plugin updated.json
 
 foreach ($lang in 'en', 'zh_CN') {
-    Invoke-WebRequest "https://purge.jsdelivr.net/gh/bs-community/plugins-dist@latest/registry_$lang.json"
+    Invoke-WebRequest "https://purge.jsdelivr.net/gh/graytoowolf/plugins-dist@latest/registry_$lang.json"
 }
-Invoke-WebRequest 'https://purge.jsdelivr.net/gh/bs-community/plugins-dist@latest/registry.json'
+Invoke-WebRequest 'https://purge.jsdelivr.net/gh/graytoowolf/plugins-dist@latest/registry.json'
