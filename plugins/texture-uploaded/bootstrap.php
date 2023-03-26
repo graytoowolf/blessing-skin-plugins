af34bf5bafa1bd0a0735a55f1c0640be05663054
graywolf <graytoowolf@outlook.com>
2023-03-26 09:55:53 +0800

    上传皮肤自动推送百度、必应

--- plugins/texture-uploaded/bootstrap.php
+++ plugins/texture-uploaded/bootstrap.php
@@ -1,11 +1,36 @@
 <?php
 
+use Illuminate\Support\Facades\Http;
 use Illuminate\Contracts\Events\Dispatcher;
 
 return function (Dispatcher $events) {
     $events->listen('texture.uploaded', function ($event) {
+        $skinurl = url('/skinlib/show') . '/' . $event->tid;
+        //百度推送
+        if (env('BAIDU_TOKEN')) {
+            //判断是否公开
+            $url = 'http://data.zz.baidu.com/urls?site=' . url('') . '&token=' . env('BAIDU_TOKEN');
+            $response = Http::withBody($skinurl, 'text/plain')->post($url);
+            if ($response->status() != 200) {
+                Log::error('百度推送失败', ['status' => $response->status(), 'body' => $response->body()]);
+            }
+        }
+        //必应推送
+        //判断env('BING_TOKEN')存在
+        if (env('BING_TOKEN')) {
+            //判断是否公开
+
+            $url = 'https://ssl.bing.com/webmaster/api.svc/json/SubmitUrlbatch?apikey=' . env('BING_TOKEN');
+            $response =  Http::post($url, [
+                'siteUrl' => url(''),
+                'urlList' => [$skinurl]
+            ]);
+            if ($response->status() != 200) {
+                Log::error('必应推送失败', ['status' => $response->status(), 'body' => $response->body()]);
+            }
+        }
+
         if ($event->public) {
-            $skinurl = url('/skinlib/show') . '/' . $event->tid;
             //添加数据到sitemap.xml
             $sitemapnema = public_path('sitemap.xml');
             $xml = simplexml_load_file($sitemapnema);
