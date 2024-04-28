e78706e1867ed2923f07f712833e0af35ae091a2
graywolf <graytoowolf@outlook.com>
2024-04-28 12:01:13 +0800

    优化bingapi

--- plugins/bing-api/src/Configuration.php
+++ plugins/bing-api/src/Configuration.php
@@ -7,20 +7,42 @@ use Illuminate\Support\Str;
 
 class Configuration
 {
+  protected $baseUrl = 'https://www.bing.com';
+  protected $endpoint = '/HPImageArchive.aspx?format=js&idx=0&n=1';
+
   public function api()
   {
-    $response = Http::get("https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1"); //从bing获取数据
-    $json = $response->json();
-    if (empty($json) || empty($json['images']) || empty($json['images'][0]['url'])) {
-      // 如果 JSON 数据为空或者不包含期望的数据结构，返回空字符串或者抛出异常
-      $replaced = 'https://www.bing.com/th?id=OHR.EdaleValley_ZH-CN8464524952_1920x1080.jpg';
-    } else {
-      $imageUrl = 'https://www.bing.com' . $json['images'][0]['url'];
-      $replaced = Str::of($imageUrl)->replace('&rf=LaDigue_1920x1080.jpg&pid=hp', ''); //替换多余字符
+    try {
+      $response = Http::get($this->baseUrl . $this->endpoint);
+      $json = $response->json();
+
+      if (empty($json['images'][0]['url'])) {
+        throw new \Exception("图片URL不存在");
+      }
+
+      $imageUrl = $this->baseUrl . $json['images'][0]['url'];
+      $filteredUrl = $this->filterImageUrl($imageUrl);
+
+      return $this->redirectWithCache($filteredUrl);
+    } catch (\Exception $e) {
+      // 日志记录异常或其他错误处理
+      report($e);
+      // 提供默认图片或错误提示
+      $defaultImageUrl = $this->baseUrl . '/th?id=OHR.EdaleValley_ZH-CN8464524952_1920x1080.jpg';
+      return $this->redirectWithCache($defaultImageUrl);
     }
-    return redirect()->away($replaced)->withHeaders([
-      'cache-control' => 'public',
-      'expires' => date(DATE_RFC7231, strtotime(gmdate('Y-m-d H:i:s')) + 1800),
+  }
+
+  protected function filterImageUrl($imageUrl)
+  {
+    return Str::of($imageUrl)->replace('&rf=LaDigue_1920x1080.jpg&pid=hp', '');
+  }
+
+  protected function redirectWithCache($url)
+  {
+    return redirect()->away($url)->withHeaders([
+      'Cache-Control' => 'public, max-age=1800',
+      'Expires' => gmdate(DATE_RFC7231, strtotime('+30 minutes'))
     ]);
   }
 }
